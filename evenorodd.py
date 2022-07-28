2e99daf41c0ef7286b4626cfb4d1108033eac9d9
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-07-27 23:45:22 -0700

    First commit

---
+++ evenorodd.py
@@ -0,0 +1,7 @@
+number = int(input("Enter any number of your choice\n"))
+remainder = number % 2
+
+if (remainder == 0):
+    print(number,"is an even number")
+else:
+    print(number,"is an odd number")   
